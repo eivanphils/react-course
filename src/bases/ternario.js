47a9cfd68653b03706d8dc09d2617270bb4c203f
Phils <phils.garcia@multicaja.cl>
2021-04-10 14:16:37 -0400

    agregado ternario

---
+++ src/bases/ternario.js
@@ -0,0 +1,14 @@
+const activo = true;
+
+// let mensaje = '';
+
+// if(activo) {
+//     mensaje = 'esta activo';
+// } else {
+//     mensaje = 'no esta activo';
+// }
+
+// const mensaje = activo ? 'esta activo' : 'no esta activo'
+const mensaje = activo && 'activo';
+
+console.log(mensaje)
